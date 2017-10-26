5d94d9883419db2e0272351b4fcca3bedd099054
Coordonator Resurse Tehnice <crt.cji@gmail.com>
2017-10-26 06:55:29 +0300

    Fixed some things

--- InitialUbuntuServerConfig.sh
+++ InitialUbuntuServerConfig.sh
@@ -2,6 +2,12 @@
 
 # Made with love to be executed on an Ubuntu 16.04 LTS droplet
 
+# Checking if the script is running as root
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Sorry, you need to run this as root"
+	exit 1
+fi
+
 # VARIABLES SECTION
 # -----------------------------------
 
@@ -24,15 +30,6 @@ blnk_echo () {
 	echo ""
 }
 
-chg_unat10 () {
-	# The following options will have unattended-upgrades check for updates every day while cleaning out the local download archive each week.
-	echo "
-	APT::Periodic::Update-Package-Lists "1";
-	APT::Periodic::Download-Upgradeable-Packages "1";
-	APT::Periodic::AutocleanInterval "7";
-	APT::Periodic::Unattended-Upgrade "1";" > $unat10;
-}
-
 # Echoes activation of a specific application option ($@)
 enbl_echo () {
   echo -e "Activating \e[1m\e[34m$@\e[0m ...";
@@ -47,21 +44,62 @@ sctn_echo () {
 	echo -e "\e[1m\e[33m$@\e[0m\n==================================================================================================";
 }
 
+chg_unat10 () {
+	# The following options will have unattended-upgrades check for updates every day while cleaning out the local download archive each week.
+	echo "
+	APT::Periodic::Update-Package-Lists "1";
+	APT::Periodic::Download-Upgradeable-Packages "1";
+	APT::Periodic::AutocleanInterval "7";
+	APT::Periodic::Unattended-Upgrade "1";" > $unat10;
+}
+
 # Backing up a given ($@) file/directory
 bckup () {
 	echo -e "Backing up: \e[1m\e[34m$@\e[0m ...";
 	cp -r $@ $@_$(date +"%m-%d-%Y")."$bckp";
 }
 
+# Updates/upgrades the system
+up () {
+  sctn_echo UPDATES;
+  upvar="update upgrade dist-upgrade";
+  for upup in $upvar; do
+    echo -e "Executing \e[1m\e[34m$upup\e[0m";
+    #apt-get -yqq $upup > /dev/null 2>&1 >> $rlog;
+    apt-get -yqq $upup >> $rlog;
+  done
+  blnk_echo;
+}
 # ------------------------------------------
 # END VARIABLES SECTION
 
 
-# Configuring UFW, updating/upgrading, installing necessary cli apps
-yes | ufw limit 22/tcp && yes | ufw limit 7539/tcp;
-ufw allow 1194/udp && ufw enable && echo "IPV6=no" >> /etc/ufw/ufw.conf && ufw reload && apt -y update && apt -y upgrade && apt -y dist-upgrade && apt -y install mc screen git whois htop arp-scan curl iptraf sysbench unattended-upgrades glances ntp ntpdate rcconf rig shellcheck sysv-rc-conf tmux;
+## UFW
+# Backing up the file
+sctn_echo FIREWALL "(UFW)";
+bckup /etc/ufw/ufw.conf;
 
-# Unattended-Upgrades configuration section
+# Limiting incomming connections to the SSH ports
+yes | ufw limit 22/tcp && yes | ufw limit 7539/tcp;
+
+# Opening UDP incoming connections for OpenVPN and enabling the firewall
+ufw allow 1194/udp && ufw enable;
+
+# Disabling IPV6 in UFW
+echo "IPV6=no" >> /etc/ufw/ufw.conf && ufw reload;
+
+
+## Updating/upgrading
+sctn_echo UPDATES;
+apt -y update && apt -y upgrade && apt -y dist-upgrade;
+
+
+## Installing necessary CLI apps
+sctn_echo INSTALLATION;
+apt -y install mc screen git whois htop arp-scan curl iptraf sysbench unattended-upgrades glances ntp ntpdate rcconf rig shellcheck sysv-rc-conf tmux;
+
+
+## Unattended-Upgrades configuration section
 sctn_echo AUTOUPDATES "(Unattended-Upgrades)";
 
 unat20=(/etc/apt/apt.conf.d/20auto-upgrades);
@@ -78,9 +116,9 @@ if [[ -f $unat20 ]] && [[ -f $unat50 ]] && [[ -f $unat10 ]]; then
 
 	# Inserting the right values into it
 	echo "
-	APT::Periodic::Update-Package-Lists "1";
-	APT::Periodic::Unattended-Upgrade "1";
-	APT::Periodic::Verbose "2";" > $unat20;
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+APT::Periodic::Verbose "2";" > $unat20;
 
 
 	# Checking if line for security updates is uncommented, by default it is
@@ -89,71 +127,71 @@ if [[ -f $unat20 ]] && [[ -f $unat50 ]] && [[ -f $unat10 ]]; then
 		chg_unat10;
 	else
 		echo "
-		// Automatically upgrade packages from these (origin:archive) pairs
-		Unattended-Upgrade::Allowed-Origins {
-			"${distro_id}:${distro_codename}";
-			"${distro_id}:${distro_codename}-security";
-			// Extended Security Maintenance; doesn't necessarily exist for
-			// every release and this system may not have it installed, but if
-			// available, the policy for updates is such that unattended-upgrades
-			// should also install from here by default.
-			"${distro_id}ESM:${distro_codename}";
-			//	"${distro_id}:${distro_codename}-updates";
-			//	"${distro_id}:${distro_codename}-proposed";
-			//	"${distro_id}:${distro_codename}-backports";
-		};
+// Automatically upgrade packages from these (origin:archive) pairs
+Unattended-Upgrade::Allowed-Origins {
+	"${distro_id}:${distro_codename}";
+	"${distro_id}:${distro_codename}-security";
+	// Extended Security Maintenance; doesn't necessarily exist for
+	// every release and this system may not have it installed, but if
+	// available, the policy for updates is such that unattended-upgrades
+	// should also install from here by default.
+	"${distro_id}ESM:${distro_codename}";
+	//	"${distro_id}:${distro_codename}-updates";
+	//	"${distro_id}:${distro_codename}-proposed";
+	//	"${distro_id}:${distro_codename}-backports";
+};
 
-		// List of packages to not update (regexp are supported)
-		Unattended-Upgrade::Package-Blacklist {
-			//	"vim";
-			//	"libc6";
-			//	"libc6-dev";
-			//	"libc6-i686";
-		};
+// List of packages to not update (regexp are supported)
+Unattended-Upgrade::Package-Blacklist {
+	//	"vim";
+	//	"libc6";
+	//	"libc6-dev";
+	//	"libc6-i686";
+};
 
-		// This option allows you to control if on a unclean dpkg exit
-		// unattended-upgrades will automatically run
-		//   dpkg --force-confold --configure -a
-		// The default is true, to ensure updates keep getting installed
-		//Unattended-Upgrade::AutoFixInterruptedDpkg "false";
+// This option allows you to control if on a unclean dpkg exit
+// unattended-upgrades will automatically run
+//   dpkg --force-confold --configure -a
+// The default is true, to ensure updates keep getting installed
+//Unattended-Upgrade::AutoFixInterruptedDpkg "false";
 
-		// Split the upgrade into the smallest possible chunks so that
-		// they can be interrupted with SIGUSR1. This makes the upgrade
-		// a bit slower but it has the benefit that shutdown while a upgrade
-		// is running is possible (with a small delay)
-		//Unattended-Upgrade::MinimalSteps "true";
+// Split the upgrade into the smallest possible chunks so that
+// they can be interrupted with SIGUSR1. This makes the upgrade
+// a bit slower but it has the benefit that shutdown while a upgrade
+// is running is possible (with a small delay)
+//Unattended-Upgrade::MinimalSteps "true";
 
-		// Install all unattended-upgrades when the machine is shuting down
-		// instead of doing it in the background while the machine is running
-		// This will (obviously) make shutdown slower
-		//Unattended-Upgrade::InstallOnShutdown "true";
+// Install all unattended-upgrades when the machine is shuting down
+// instead of doing it in the background while the machine is running
+// This will (obviously) make shutdown slower
+//Unattended-Upgrade::InstallOnShutdown "true";
 
-		// Send email to this address for problems or packages upgrades
-		// If empty or unset then no email is sent, make sure that you
-		// have a working mail setup on your system. A package that provides
-		// 'mailx' must be installed. E.g. "user@example.com"
-		//Unattended-Upgrade::Mail "root";
+// Send email to this address for problems or packages upgrades
+// If empty or unset then no email is sent, make sure that you
+// have a working mail setup on your system. A package that provides
+// 'mailx' must be installed. E.g. "user@example.com"
+//Unattended-Upgrade::Mail "root";
 
-		// Set this value to "true" to get emails only on errors. Default
-		// is to always send a mail if Unattended-Upgrade::Mail is set
-		//Unattended-Upgrade::MailOnlyOnError "true";
+// Set this value to "true" to get emails only on errors. Default
+// is to always send a mail if Unattended-Upgrade::Mail is set
+//Unattended-Upgrade::MailOnlyOnError "true";
 
-		// Do automatic removal of new unused dependencies after the upgrade
-		// (equivalent to apt-get autoremove)
-		//Unattended-Upgrade::Remove-Unused-Dependencies "false";
+// Do automatic removal of new unused dependencies after the upgrade
+// (equivalent to apt-get autoremove)
+//Unattended-Upgrade::Remove-Unused-Dependencies "false";
 
-		// Automatically reboot *WITHOUT CONFIRMATION*
-		//  if the file /var/run/reboot-required is found after the upgrade
-		//Unattended-Upgrade::Automatic-Reboot "false";
+// Automatically reboot *WITHOUT CONFIRMATION*
+//  if the file /var/run/reboot-required is found after the upgrade
+//Unattended-Upgrade::Automatic-Reboot "false";
 
-		// If automatic reboot is enabled and needed, reboot at the specific
-		// time instead of immediately
-		//  Default: "now"
-		//Unattended-Upgrade::Automatic-Reboot-Time "02:00";
+// If automatic reboot is enabled and needed, reboot at the specific
+// time instead of immediately
+//  Default: "now"
+//Unattended-Upgrade::Automatic-Reboot-Time "02:00";
 
-		// Use apt bandwidth limit feature, this example limits the download
-		// speed to 70kb/sec
-		//Acquire::http::Dl-Limit "70";" > $unat50;
+// Use apt bandwidth limit feature, this example limits the download
+// speed to 70kb/sec
+//Acquire::http::Dl-Limit "70";" > $unat50;
 
 		chg_unat10;
 	fi
@@ -161,7 +199,7 @@ if [[ -f $unat20 ]] && [[ -f $unat50 ]] && [[ -f $unat10 ]]; then
 	# The results of unattended-upgrades will be logged to /var/log/unattended-upgrades.
 	# For more tweaks nano /etc/apt/apt.conf.d/50unattended-upgrades
 
-	blnk_echo > $dn >> $rlog;
+	blnk_echo >> $rlog;
 
 else
 	nofile_echo $unat20 or $unat50 or $unat10;
@@ -175,12 +213,12 @@ fi
 # clmcnf=(/etc/clamav/freshclam.conf);
 # rprtfldr=(/root/ClamAV-Reports);
 #
-# sctn_echo ANTIVIRUS "(Clam-AV)" > $dn >> $rlog;
+# sctn_echo ANTIVIRUS "(Clam-AV)" >> $rlog;
 # bckup $clmcnf;
 # mkdir -p $rprtfldr;
 #
 # # Enabling "SafeBrowsing true" mode
-# enbl_echo SafeBrowsing > $dn >> $rlog;
+# enbl_echo SafeBrowsing >> $rlog;
 # echo "SafeBrowsing true" >> $clmcnf;
 #
 # # Restarting CLAMAV Daemons
@@ -189,8 +227,8 @@ fi
 #
 # # Scanning the whole system and palcing all the infected files list on a particular file
 # # echo "ClamAV is scanning the OS ...";
-# scn_echo ClamAv > $dn >> $rlog;
-# # This one throws any kind of warnings and errors: clamscan -r / | grep FOUND >> $rprtfldr/clamscan_first_scan.txt > $dn >> $rlog;
+# scn_echo ClamAv >> $rlog;
+# # This one throws any kind of warnings and errors: clamscan -r / | grep FOUND >> $rprtfldr/clamscan_first_scan.txt >> $rlog;
 # clamscan --recursive --no-summary --infected / 2>/dev/null | grep FOUND >> $rprtfldr/clamscan_first_scan.txt;
 # # Crontab: The daily scan
 #
@@ -206,10 +244,10 @@ fi
 # # 30 01 * * * /usr/bin/freshclam --quiet; /usr/bin/clamscan --recursive --no-summary --infected / 2>/dev/null >> $rprtfldr/clamscan_daily.txt"; } | crontab -
 #
 # # This way, Anacron ensures that if the computer is off during the time interval when it is supposed to be scanned by the daemon, it will be scanned next time it is turned on, no matter today or another day.
-# echo -e "Creating a \e[1m\e[34mcronjob\e[0m for the ClamAV ..." > $dn >> $rlog;
+# echo -e "Creating a \e[1m\e[34mcronjob\e[0m for the ClamAV ..." >> $rlog;
 # echo -e '#!/bin/bash\n\n/usr/bin/freshclam --quiet;\n/usr/bin/clamscan --recursive --exclude-dir=/media/ --no-summary --infected / 2>/dev/null >> '$rprtfldr'/clamscan_daily_$(date +"%m-%d-%Y").txt;' >> /etc/cron.daily/clamscan.sh && chmod 755 /etc/cron.daily/clamscan.sh;
 #
-# blnk_echo > $dn >> $rlog;
+# blnk_echo >> $rlog;
 #
 # # END: ClamAV section: configuration and the first scan
 
